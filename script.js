112acf6d1a144775ad5439a2f3519f96e56a705c
sachin7con <86023048+sachin7con@users.noreply.github.com>
2024-03-18 12:14:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
 function getFormvalue() {
-    //Write your code here
-
+    //SGN Write your code here
+	let sub = document.getElementById("form1")
+		sub.onsubmit = function(event){
+			event.prevernDefault;
+			myFunction();}
+	function myFunction() {
+		let fN = sub.elements["fname"].value;
+		let lN = sub.elements["lname"].value; 
+		alert(fN + " " + lN);
+	}
+	
 }
+getFormvalue();
